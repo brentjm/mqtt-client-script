80baff6e7415bfe252d9f3b745e81c5182cd4870
brentjm <brent_maranzano@yahoo.com>
2020-06-19 12:55:30 +0100

    Changed container to restart. For a Docker restart policy of no, the container does not start on boot. Thus, the restar policy was changed to always. To prevent the script from exiting, a while loop was inserted into the script (infinite loop). The script also sends a date MQTT message every day.

--- publish_status.sh
+++ publish_status.sh
@@ -3,3 +3,10 @@ IP=$(ip -4 address show eth0 | grep inet | awk '{print $2}')
 echo "$IP"
 mosquitto_pub -h 10.131.72.83 -t "ape/7/ip" -m "$IP" -r
 mosquitto_pub -h 10.131.72.83 -t "ape/7/location" -m "DPD" -r
+DATE=$(date)
+while true
+do
+    mosquitto_pub -h 10.131.72.83 -t "ape/7/status" -m "$DATE" -r
+    sleep 1d
+done
+    
